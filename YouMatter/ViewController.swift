5a9e43a970ddb7979fb2cce12d68edac215be95d
Tracey Carter <traceycarterdbc@gmail.com>
2015-10-06 21:02:28 -0700

    first commit for final project YouMatter

--- YouMatter/ViewController.swift
+++ YouMatter/ViewController.swift
@@ -7,13 +7,57 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
+    @IBOutlet var map: MKMapView!
+    
+    var locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = NSURL(string: "https://www.domesticshelters.org/search#?q=666%20Folsom%20St%2C%20San%20Francisco%2C%20CA%2094107%2C%20USA&latitude=37.78462700000001&longitude=-122.398458&radius=50&order=distance&page=1")!
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url) {(data, response, error) -> Void in
+        }
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        
+    
+
     }
+    
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+            print(locations)
+            var userLocation: CLLocation = locations[0]
+            
+            var latitude = userLocation.coordinate.latitude
+            
+            var longitude = userLocation.coordinate.longitude
+            
+            var latDelta:CLLocationDegrees = 0.05
+            
+            var lonDelta:CLLocationDegrees = 0.05
+            
+            var span:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, lonDelta)
+            
+            var location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+            
+            var region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+            
+            self.map.setRegion(region, animated: false)
+            
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
